9f462b8b5843b3f8febc105f0235e3b00eceb83b
elza akhmatyanova <akh24071998@yandex.ru>
2025-05-17 15:19:50 +0500

    editing JsonParser

--- app/src/main/java/hexlet/code/parsers/JsonParser.java
+++ app/src/main/java/hexlet/code/parsers/JsonParser.java
@@ -9,14 +9,14 @@ import java.nio.file.Paths;
 import java.util.Map;
 
 public class JsonParser {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     public static Map<String, Object> parse (String filePath) throws Exception {
         Path fileAbsPath = Paths.get(filePath).toAbsolutePath().normalize();
         if (!Files.exists(fileAbsPath)) {
             throw new Exception("File '" + fileAbsPath + "' does not exist");
         }
         String fileContent = Files.readString(fileAbsPath);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(fileContent, new TypeReference<Map<String,Object>>(){});
+        return mapper.readValue(fileContent, new TypeReference<Map<String,Object>>(){});
     }
 }
