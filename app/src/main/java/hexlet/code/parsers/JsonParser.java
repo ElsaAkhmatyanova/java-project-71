69b7e230206db55a24e1779db019b3ebdcf77297
elza akhmatyanova <akh24071998@yandex.ru>
2025-05-17 14:58:01 +0500

    adding JsonParser class for reading and parsing json

---
+++ app/src/main/java/hexlet/code/parsers/JsonParser.java
@@ -0,0 +1,22 @@
+package hexlet.code.parsers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class JsonParser {
+    public static Map<String, Object> parse (String filePath) throws Exception {
+        Path fileAbsPath = Paths.get(filePath).toAbsolutePath().normalize();
+        if (!Files.exists(fileAbsPath)) {
+            throw new Exception("File '" + fileAbsPath + "' does not exist");
+        }
+        String fileContent = Files.readString(fileAbsPath);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(fileContent, new TypeReference<Map<String,Object>>(){});
+    }
+}
