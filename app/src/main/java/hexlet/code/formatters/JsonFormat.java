e65831ee6d6d128d9e7428017dfe5b7c2577290e
elza akhmatyanova <akh24071998@yandex.ru>
2025-06-25 18:48:03 +0500

    moving JsonFormat and Formatter into formatters

--- app/src/main/java/hexlet/code/formatters/JsonFormat.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormat.java
@@ -1,14 +1,22 @@
-package hexlet.code.parsers;
+package hexlet.code.formatters;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.DiffEntry;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 
-public class JsonParser {
-    private static final ObjectMapper mapper = new ObjectMapper();
+public class JsonFormat implements Formatter{
+
+    @Override
+    public String format(List<DiffEntry> diff) throws JsonProcessingException {
+        return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(diff);
+    }
 
     public static Map<String, Object> parse(String filePath) throws Exception {
         Path fileAbsPath = Paths.get(filePath).toAbsolutePath().normalize();
