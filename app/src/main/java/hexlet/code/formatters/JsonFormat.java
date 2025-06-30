8159e2709c2b52e3a14cd4e0bfe8d159cea24832
elza akhmatyanova <akh24071998@yandex.ru>
2025-06-30 23:29:08 +0500

    adding methods format in JsonFormat

--- app/src/main/java/hexlet/code/formatters/JsonFormat.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormat.java
@@ -11,20 +11,15 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
-public class JsonFormat implements Formatter{
+public class JsonFormat implements Formatter {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Override
-    public String format(List<DiffEntry> diff) throws JsonProcessingException {
-        return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(diff);
-    }
-
-    public static Map<String, Object> parse(String filePath) throws Exception {
-        Path fileAbsPath = Paths.get(filePath).toAbsolutePath().normalize();
-        if (!Files.exists(fileAbsPath)) {
-            throw new Exception("File '" + fileAbsPath + "' does not exist");
+    public String format(List<DiffEntry> diffEntries) {
+        try {
+            return MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(diffEntries);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to convert diff to JSON", e);
         }
-        String fileContent = Files.readString(fileAbsPath);
-        return mapper.readValue(fileContent, new TypeReference<Map<String, Object>>() {
-        });
     }
 }
