e17a78d65ca20f4609c01af97a6b7ce9fd4f940a
elza akhmatyanova <akh24071998@yandex.ru>
2025-07-08 20:18:01 +0500

    refactor imports

--- app/src/main/java/hexlet/code/formatters/JsonFormat.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormat.java
@@ -1,15 +1,10 @@
 package hexlet.code.formatters;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.DiffEntry;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
 
 public class JsonFormat implements Formatter {
     private static final ObjectMapper MAPPER = new ObjectMapper();
