fcf0926d5461b656eca8ef72c6dfa982994b423b
elza akhmatyanova <akh24071998@yandex.ru>
2025-07-17 20:38:29 +0500

    refactor code

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -12,14 +12,6 @@ public class Parser {
     private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
     private static final ObjectMapper YAML_MAPPER = new ObjectMapper(new YAMLFactory());
 
-    public static Map<String, Object> parse(String content, String dataType) throws IOException {
-        return switch (dataType) {
-            case "json" -> JSON_MAPPER.readValue(content, new TypeReference<>() {});
-            case "yaml", "yml" -> YAML_MAPPER.readValue(content, new TypeReference<>() {});
-            default -> throw new IllegalArgumentException("Unsupported format: " + dataType);
-        };
-    }
-
     public static Map<String, Object> parseFromFile(String path) throws IOException {
         String content = Files.readString(Path.of(path));
         String ext = path.substring(path.lastIndexOf('.') + 1);
