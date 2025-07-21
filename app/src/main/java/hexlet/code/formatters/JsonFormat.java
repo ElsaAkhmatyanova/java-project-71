2971ebc6be50e9864be37c89955191288d56b80a
elza akhmatyanova <akh24071998@yandex.ru>
2025-07-21 21:33:09 +0500

    remove exception catching in JsonFormat

--- app/src/main/java/hexlet/code/formatters/JsonFormat.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormat.java
@@ -1,19 +1,15 @@
 package hexlet.code.formatters;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.DiffEntry;
+
 import java.util.List;
 
 public final class JsonFormat implements Formatter {
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Override
-    public String format(List<DiffEntry> diffEntries) {
-        try {
-            return MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(diffEntries);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to convert diff to JSON", e);
-        }
+    public String format(List<DiffEntry> diffEntries) throws Exception {
+        return MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(diffEntries);
     }
 }
