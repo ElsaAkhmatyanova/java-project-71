eb0bbe606b9bf7fc189fcf91249435251d35dc93
elza akhmatyanova <akh24071998@yandex.ru>
2025-07-21 21:28:18 +0500

    remove lombok dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,10 +34,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.25.3")
-    compileOnly("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
-    testCompileOnly("org.projectlombok:lombok:1.18.32")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
 }
 
 checkstyle {
