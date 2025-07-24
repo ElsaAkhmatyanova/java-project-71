af28ca7df3d8e5b7e0b9f56194089ad553c77569
elza akhmatyanova <akh24071998@yandex.ru>
2025-07-24 23:08:47 +0500

    refactor tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -10,18 +10,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DifferTest {
-    private static final String JSON_FILE_PATH_1 = "fixtures/files/file1.json";
-    private static final String JSON_FILE_PATH_2 = "fixtures/files/file2.json";
-    private static final String YML_FILE_PATH_1 = "fixtures/files/file1.yml";
-    private static final String YML_FILE_PATH_2 = "fixtures/files/file2.yml";
-
     private static String stylishExpectedForJson;
     private static String plainExpectedForJson;
     private static String jsonExpectedForJson;
     private static String stylishExpectedForYml;
+    private static String plainExpectedForYml;
+    private static String jsonExpectedForYml;
 
     private static String readFile(String resourcesFilePath) {
         try {
@@ -43,32 +40,37 @@ class DifferTest {
         }
     }
 
-    @BeforeAll
-    static void setUp() {
-        stylishExpectedForJson = readFile("fixtures/expected_results/stylish_result_for_json_file.txt");
-        plainExpectedForJson = readFile("fixtures/expected_results/plain_result_for_json_file.txt");
-        jsonExpectedForJson = readFile("fixtures/expected_results/json_result_for_json_file.txt");
-        stylishExpectedForYml = readFile("fixtures/expected_results/stylish_result_for_yml_file.txt");
+    private String getFixturePathString(String fileName) {
+        return getResourseFilePath("fixtures/files/" + fileName).toAbsolutePath().normalize().toString();
     }
 
-    static Stream<Arguments> formatProvider() {
+    @BeforeAll
+    static void setUp() {
+        stylishExpectedForJson = readFile("fixtures/expected_results/stylish_result_for_json.txt");
+        plainExpectedForJson = readFile("fixtures/expected_results/plain_result_for_json.txt");
+        jsonExpectedForJson = readFile("fixtures/expected_results/json_result_for_json.txt");
+        stylishExpectedForYml = readFile("fixtures/expected_results/stylish_result_for_yml.txt");
+        plainExpectedForYml = readFile("fixtures/expected_results/plain_result_for_yml.txt");
+        jsonExpectedForYml = readFile("fixtures/expected_results/json_result_for_yml.txt");
+    }
+
+    static Stream<Arguments> testCaseProvider() {
         return Stream.of(
-                Arguments.of(JSON_FILE_PATH_1, JSON_FILE_PATH_2, "stylish", stylishExpectedForJson),
-                Arguments.of(JSON_FILE_PATH_1, JSON_FILE_PATH_2, "plain", plainExpectedForJson),
-                Arguments.of(JSON_FILE_PATH_1, JSON_FILE_PATH_2, "json", jsonExpectedForJson),
-                Arguments.of(JSON_FILE_PATH_1, JSON_FILE_PATH_2, null, stylishExpectedForJson),
-                Arguments.of(YML_FILE_PATH_1, YML_FILE_PATH_2, "stylish", stylishExpectedForYml)
+                Arguments.of("json", stylishExpectedForJson, plainExpectedForJson, jsonExpectedForJson),
+                Arguments.of("yml", stylishExpectedForYml, plainExpectedForYml, jsonExpectedForYml)
         );
     }
 
     @ParameterizedTest
-    @MethodSource("formatProvider")
-    void testGenerateDiffWithFormats(String path1, String path2, String format, String expected) throws Exception {
-        String actual = Differ.generate(
-                getResourseFilePath(path1).toAbsolutePath().normalize().toString(),
-                getResourseFilePath(path2).toAbsolutePath().normalize().toString(),
-                format
-        );
-        assertEquals(expected.trim(), actual.trim());
+    @MethodSource("testCaseProvider")
+    void testGenerateDiffWithFormats(String format, String stylishExpected, String plainExpected, String jsonExpected)
+            throws Exception {
+        String filePath1 = getFixturePathString("file1." + format);
+        String filePath2 = getFixturePathString("file2." + format);
+
+        assertThat(Differ.generate(filePath1, filePath2, "stylish")).isEqualTo(stylishExpected);
+        assertThat(Differ.generate(filePath1, filePath2)).isEqualTo(stylishExpected);
+        assertThat(Differ.generate(filePath1, filePath2, "plain")).isEqualTo(plainExpected);
+        assertThat(Differ.generate(filePath1, filePath2, "json")).isEqualTo(jsonExpected);
     }
 }
